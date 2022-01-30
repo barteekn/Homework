0f4965cfadfb13812e884102867737da28dab773
Bartek Nowakowski <nowakowskib@gmail.com>
2022-01-30 22:06:56 +0100

    update on nodeJS server

--- 04-homework-grid-animation-node/js/server.js
+++ 04-homework-grid-animation-node/js/server.js
@@ -1,9 +1,29 @@
-const http = require('http')
-const fs = require('fs')
-
-const server = http.createServer((req, res) =>{
-  res.writeHead(200, {'content-type': 'text/html'})
-  fs.createReadStream('index.html').pipe(res)
-})
-
-server.listen(5500, '127.0.0.1')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+ 
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, "index.html"), (err, page) => {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+      res.write('<h1>Index html</h1>');
+      res.end(page)
+    })
+  }
+  else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>About html</h1>');
+    res.end();
+  }
+  else if (req.url === '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>Contact html</h1>');
+    res.end();
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8'});
+    res.end();
+  }
+});
+ 
+server.listen(5000, '127.0.0.1', () => console.log('Server Listen'));
