974931f89d3cffafe99f43c91a028f732eccaf49
Bartek Nowakowski <nowakowskib@gmail.com>
2022-01-30 21:20:37 +0100

    update nodeJS

--- 04-homework-grid-animation-node/js/script.js
+++ 04-homework-grid-animation-node/js/script.js
@@ -6,15 +6,20 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") {
     fs.readFile(path.join(__dirname, "index.html"), (err, page) => {
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.write("<h1>Index-html</h1>");
       res.end(page);
     });
   }
-
-  if (req.url === "/about") {
+  if (req.url === "/about.html") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write("<h1>O nas</h1>");
+    res.write("<h1>O nas-html</h1>");
+    res.end();
+  }
+  if (req.url === "/contact.html") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<h1>Kontakt-html</h1>");
     res.end();
   }
 });
 
-server.listen(5500, "127.0.0.1", () => console.log("Server Listen"));
+server.listen(5000, "127.0.0.1", () => console.log("Server Listen"));
