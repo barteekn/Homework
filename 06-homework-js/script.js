181e7016f8a8994f50b232aa05599ea347d78b7c
Bartek Nowakowski <nowakowskib@gmail.com>
2022-02-20 14:43:12 +0100

    update 06 homework

--- 06-homework-js/script.js
+++ 06-homework-js/script.js
@@ -1,18 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ZAD 1
+
 const students = [
     {
-       id: 1,
+       id: 0,
        pesel: '123456789',
        name: "Jacek Kowalski",
        notes: [5, 3, 4, 2, 5, 5]
     },
     {
-       id: 2,
+       id: 1,
        pesel: '234567890',
        name: "Ewa Nowak",
        notes: [2, 3, 3, 3, 2, 5]
     },
     {
-       id: 3,
+       id: 2,
        pesel: '345678901',
        name: "Zygmunt Lewandowski",
        notes: [2, 2, 4, 4, 3, 3]
@@ -20,14 +23,14 @@ const students = [
    ];
 
     studentsAdd = {
-        id: 4,
+        id: 3,
         pesel: '456789012',
         name: "Bartek Cebulski",
         notes: [4, 4, 6, 5, 2, 3]
      };
 
      studentsAdd1 = {
-        id: 5,
+        id: 4,
         pesel: '567890123',
         name: "Jadwiga Zelman",
         notes: [5, 5, 4, 3, 2, 1]
@@ -38,15 +41,15 @@ const students = [
 
 //////////////////////////////////////////////////////////////////////////////// 
 
-const getMaxNote = (studentsArray, studnetId, callback) => {
-   for (let i = 0; i < studentsArray.length; i++) {
-      if (studnetId === studentsArray[i].pesel) {
+const getMaxNote = (students, studnetId, callback) => {
+   for (let i = 0; i < students.length; i++) {
+      if (studnetId === students[i].pesel) {
        return studnetId = i;
       }
-      const max = Math.max(...studentsArray[studnetId].notes);
+      const max = Math.max(...students[i].notes);
       console.log(max);
 
-      return callback(studentsArray[i], max, 'max')
+      return callback(students[i], max, 'max')
    }
 };
 
@@ -59,14 +62,61 @@ const displayInfo = (studentName, note, check) => {
       
 };
 
-const getMinNote = (studentsArray, studnetId, callback) => {
-   for (let i=0; i < studentsArray.length; i++) {
-      if (studnetId === studentsArray[i].pesel) {
+const getMinNote = (students, studnetId, callback) => {
+   for (let i = 0; i < students.length; i++) {
+      if (studnetId === students[i].pesel) {
       return;
       }
-      const min = Math.min(...studentsArray[i].notes);
+      const min = Math.min(...students[i].notes);
       console.log(min);
 
-      return callback(studentsArray[i], min)
+      return callback(students[i], min)
       }
  };
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ZAD 2
+
+// const studentsList = [
+
+//    {
+//       id: 0,
+//       pesel: '123456789',
+//       name: "Jacek Kowalski",
+//       notes: [5, 3, 4, 2, 5, 5]
+//    },
+
+//    {
+//       id: 1,
+//       pesel: '234567890',
+//       name: "Ewa Nowak",
+//       notes: [2, 3, 3, 3, 2, 5]
+//    },
+
+//    {
+//       id: 2,
+//       pesel: '345678901',
+//       name: "Zygmunt Lewandowski",
+//       notes: [2, 2, 4, 4, 3, 3]
+//    },
+
+//   {
+//        id: 3,
+//        pesel: '456789012',
+//        name: "Bartek Cebulski",
+//        notes: [4, 4, 6, 5, 2, 3]
+//     },
+
+//    {
+//        id: 4,
+//        pesel: '567890123',
+//        name: "Jadwiga Zelman",
+//        notes: [5, 5, 4, 3, 2, 1]
+//     }
+
+//    ];
+
+//    studentsList.sort(function (name, student2) {
+//       return student1.split(' ')[1] > student2.split(' ')[1];
+//    });
+//    console.log (studentsList);
