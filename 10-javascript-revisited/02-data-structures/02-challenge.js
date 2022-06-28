7e2a3131d7bd484fb7f6430c968c162f91060061
Bartek Nowakowski <nowakowskib@gmail.com>
2022-06-28 16:02:18 +0200

    Challenge02DataStructures-DONE

--- 10-javascript-revisited/02-data-structures/02-challenge.js
+++ 10-javascript-revisited/02-data-structures/02-challenge.js
@@ -1,4 +1,5 @@
 // Challenge 2 - Czy zostanę Junior Developerem?
+// Nowa funkcja odkomentowana, stara zakomentowana.
 
 const students = [
   { name: "Zbigniew", tasksDone: 150, hoursSpent: 160 },
@@ -6,8 +7,6 @@ const students = [
   { name: "Krzysztof", tasksDone: 100, hoursSpent: 80 },
   { name: "Seba", tasksDone: 200, hoursSpent: 40 },
   { name: "Dagmara", tasksDone: 50, hoursSpent: 100 },
-  { name: "Kasia", tasksDone: 150, hoursSpent: 40 },
-  { name: "Bartek", tasksDone: 116, hoursSpent: 103 },
 ];
 
 const requirements = {
@@ -15,16 +14,45 @@ const requirements = {
   minTasksDone: 100,
 };
 
-function isJuniorDeveloper(student) {
-  return (
-    student.hoursSpent >= requirements.minHoursSpent &&
-    student.tasksDone >= requirements.minTasksDone
-  );
+function checkJuniorReq (students, requirements) {
+  let juniorStudents = [];
+  let seniorStudents = [];
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].hoursSpent >= requirements.minHoursSpent && students[i].tasksDone >= requirements.minTasksDone) {
+      juniorStudents.push(students[i].name);
+    } else {
+      seniorStudents.push(students[i].name);
+    }
+  }
+  return `${juniorStudents.join(', ')} - gratulacje! Ciężka praca popłaca! ${seniorStudents.join(', ')} - DO ROBOTY LENIE!`;
 }
+console.log(checkJuniorReq(students, requirements));
 
-function checkJuniorReq(students) {
-  return students.filter(isJuniorDeveloper);
-}
+// const students = [
+//   { name: "Zbigniew", tasksDone: 150, hoursSpent: 160 },
+//   { name: "Krystyna", tasksDone: 1, hoursSpent: 1 },
+//   { name: "Krzysztof", tasksDone: 100, hoursSpent: 80 },
+//   { name: "Seba", tasksDone: 200, hoursSpent: 40 },
+//   { name: "Dagmara", tasksDone: 50, hoursSpent: 100 },
+//   { name: "Kasia", tasksDone: 150, hoursSpent: 40 },
+//   { name: "Bartek", tasksDone: 116, hoursSpent: 103 },
+// ];
 
-console.log(students.filter(isJuniorDeveloper));
-console.log(checkJuniorReq(students));
+// const requirements = {
+//   minHoursSpent: 80,
+//   minTasksDone: 100,
+// };
+
+// function isJuniorDeveloper(student) {
+//   return (
+//     student.hoursSpent >= requirements.minHoursSpent &&
+//     student.tasksDone >= requirements.minTasksDone
+//   );
+// }
+
+// function checkJuniorReq(students) {
+//   return students.filter(isJuniorDeveloper);
+// }
+
+// console.log(students.filter(isJuniorDeveloper));
+// console.log(checkJuniorReq(students));
